e38cf6569cc6117a21a0b7a2710331760de80bbd
Sevian <sevianrhyt@gmail.com>
2025-04-12 00:12:25 +0700

    First commit - upload Laravel project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Beranda')
+
+@section('content')
+  <main id="main">
+    <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+      <div class="hero-container" data-aos="fade-in">
+        <h1>Selamat Datang</h1>
+        <p>di <span class="typed" data-typed-items="iPortfolio, Laravel App, Bootstrap Template"></span></p>
+      </div>
+    </section>
+  </main>
+@endsection
